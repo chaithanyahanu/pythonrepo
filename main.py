3fda6b9acf4042654c4667627c4ec4a9e7160c6d
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2026-04-03 14:51:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello from FastAPI new3"}
+    return {"message": "Hello from FastAPI new4"}
 
 @app.get("/health")
 def health_check():
